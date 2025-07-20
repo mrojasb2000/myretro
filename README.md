d056caaf4119ce30a452da83506ee252253b8ad9
Mauricio Rojas <mrojasb2000@gmail.com>
2025-07-20 16:54:56 -0400

    chore: Add application runner, command line runner and application ready events

--- README.md
+++ README.md
@@ -4,6 +4,12 @@
 ./gradlew clean build
 ```
 
+#### Running My Retro Application
+```shell
+./gradlew bootRun --args="--enable --remote --option=value1 --option=value2 update upgrade" 
+```
+
+
 ### Execute My Retro Application
 ```shell
 java -jar build/libs/myretro-0.0.1-SNAPSHOT.jar --enable --remote --option=value1 --option=value2 update upgrade
--- src/main/kotlin/com/bramwork/myretro/MyRetroConfiguration.kt
+++ src/main/kotlin/com/bramwork/myretro/MyRetroConfiguration.kt
@@ -3,15 +3,37 @@ package com.bramwork.myretro
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.ApplicationListener
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class MyRetroConfiguration(arguments: ApplicationArguments) {
+class MyRetroConfiguration() {
     var log: Logger = LoggerFactory.getLogger(MyretroApplication::class.java)
 
-    init {
-        log.info("Option Args: {}", arguments.optionNames)
-        log.info("Option Arg Values: {}", arguments.getOptionValues("option"))
-        log.info("Non Option: {}", arguments.nonOptionArgs)
+    @Bean
+    fun commandLineRunner(): CommandLineRunner {
+        return CommandLineRunner { args ->
+            log.info("[CLR] Args: {}", args.contentToString())
+        }
     }
+
+    @Bean
+    fun applicationRunner(): ApplicationRunner {
+        return ApplicationRunner {
+            args: ApplicationArguments ->
+            log.info("[AR] Option Args: {}", args.optionNames)
+            log.info("[AR] Option Arg Values: {}", args.getOptionValues("option"))
+            log.info("[AR] Non Option: {}", args.nonOptionArgs)
+        }
+    }
+
+    @Bean
+    fun applicationReadyEventApplicationListener(): ApplicationListener<ApplicationReadyEvent> {
+        return ApplicationListener { event: ApplicationReadyEvent? -> log.info("[AL] Im ready to interact...")}
+    }
+
 }
