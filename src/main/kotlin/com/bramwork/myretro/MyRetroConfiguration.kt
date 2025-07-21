bc1b8a534da859b7bea723a955b12df8b2f7591b
Mauricio Rojas <mrojasb2000@gmail.com>
2025-07-20 22:04:55 -0400

    chore: add get value from application.properties

--- src/main/kotlin/com/bramwork/myretro/MyRetroConfiguration.kt
+++ src/main/kotlin/com/bramwork/myretro/MyRetroConfiguration.kt
@@ -1,39 +1,40 @@
 package com.bramwork.myretro
 
-import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.boot.ApplicationArguments
-import org.springframework.boot.ApplicationRunner
-import org.springframework.boot.CommandLineRunner
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.ApplicationListener
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
+
 @Configuration
 class MyRetroConfiguration() {
-    var log: Logger = LoggerFactory.getLogger(MyretroApplication::class.java)
+    var log = LoggerFactory.getLogger(MyretroApplication::class.java)
+
+    @Value("\${users.server}")
+    var server: String? = null
+
+    @Value("\${users.port}")
+    var port: Int? = null
+
+    @Value("\${users.username}")
+    var username: String? = null
+
+    @Value("\${users.password}")
+    var password: String? = null
 
     @Bean
-    fun commandLineRunner(): CommandLineRunner {
-        return CommandLineRunner { args ->
-            log.info("[CLR] Args: {}", args.contentToString())
+    fun init(): ApplicationListener<ApplicationReadyEvent> {
+        return ApplicationListener { event: ApplicationReadyEvent? ->
+            log.info(
+                "\nThe users service properties are:\n- Server: {}\n- Port: {}\n- Username: {}\n- Password: {}"
+                ,server
+                ,port
+                ,username
+                ,password
+            )
         }
-    }
-
-    @Bean
-    fun applicationRunner(): ApplicationRunner {
-        return ApplicationRunner {
-            args: ApplicationArguments ->
-            log.info("[AR] Option Args: {}", args.optionNames)
-            log.info("[AR] Option Arg Values: {}", args.getOptionValues("option"))
-            log.info("[AR] Non Option: {}", args.nonOptionArgs)
-        }
-    }
-
-    @Bean
-    fun applicationReadyEventApplicationListener(): ApplicationListener<ApplicationReadyEvent> {
-        return ApplicationListener { event: ApplicationReadyEvent? -> log.info("[AL] Im ready to interact...")}
     }
 
 }
