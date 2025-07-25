5f8401e0b5031556d91bdcb1d8f1d3db28199fee
Mauricio Rojas <mrojasb2000@gmail.com>
2025-07-25 10:39:10 -0400

    chore: add testing retro board service

--- src/main/kotlin/com/bramwork/myretro/persistence/RetroBoardRepository.kt
+++ src/main/kotlin/com/bramwork/myretro/persistence/RetroBoardRepository.kt
@@ -9,17 +9,17 @@ import java.util.UUID
 @Component
 class RetroBoardRepository : BaseRepository<RetroBoard, UUID> {
     private val retroBoardMap = mutableMapOf(
-        UUID.fromString("9DC9B71B-A07E-418B-B972-40225449AFF2") to RetroBoard(
-            id = UUID.fromString("9DC9B71B-A07E-418B-B972-40225449AFF2"),
+        UUID.fromString("b79cb3ba-745e-5d9a-8903-4a02327a7e09") to RetroBoard(
+            id = UUID.fromString("b79cb3ba-745e-5d9a-8903-4a02327a7e09"),
             name = "Spring Boot 3.0 Meeting",
             cards = listOf(
-                Card(id = UUID.fromString("BB2A80A5-A0F5-4180-A6DC-80C84BC014C9"),
+                Card(id = UUID.fromString("20354d7a-e4fe-47af-8ff6-187bca92f3f9"),
                     comment = "Happy to meet the team", cardType = CardType.HAPPY),
-                Card(id = UUID.fromString("011EF086-7645-4534-9512-B9BC4CCFB688"),
+                Card(id = UUID.fromString("bd2cbad1-6ccf-48e3-bb92-bc9961bc011e"),
                     comment = "New Projects", cardType = CardType.HAPPY),
-                Card(id = UUID.fromString("775A3905-D6BE-49AB-A3C4-EBE287B51539"),
+                Card(id = UUID.fromString("b11c9be1-b619-4ef5-be1b-a1cd9ef265b7"),
                     comment = "When to meet again??", cardType = CardType.MEH),
-                Card(id = UUID.fromString("896C093D-1C50-49A3-A58A6F1008789632"),
+                Card(id = UUID.fromString("caa8b54a-eb5e-4134-8ae2-a3946a428ec7"),
                     comment = "We need more time to finish", cardType = CardType.SAD)
             )
         )
--- src/test/kotlin/com/bramwork/myretro/MyretroApplicationTests.kt
+++ src/test/kotlin/com/bramwork/myretro/MyretroApplicationTests.kt
@@ -1,13 +1,103 @@
 package com.bramwork.myretro
 
+import com.bramwork.myretro.board.Card
+import com.bramwork.myretro.board.CardType
+import com.bramwork.myretro.board.RetroBoard
+import com.bramwork.myretro.exception.CardNotFoundException
+import com.bramwork.myretro.exception.RetroBoardNotFoundException
+import com.bramwork.myretro.service.RetroBoardService
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.AssertionsForClassTypes
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import java.util.UUID
 
 @SpringBootTest
-class MyretroApplicationTests {
+internal class MyretroApplicationTests {
+	@Autowired
+	lateinit var service: RetroBoardService
+
+	var retroBoardUUID = UUID.fromString("b79cb3ba-745e-5d9a-8903-4a02327a7e09")
+	var cardUUID = UUID.fromString("20354d7a-e4fe-47af-8ff6-187bca92f3f9")
+	var mehCardUUID = UUID.fromString("b11c9be1-b619-4ef5-be1b-a1cd9ef265b7")
+
 
 	@Test
-	fun contextLoads() {
+	fun saveRetroBoardTest() {
+		val retroBoard = service.save(RetroBoard(name = "Gathering 2023"))
+		Assertions.assertThat(retroBoard).isNotNull()
+		Assertions.assertThat(retroBoard.id!!).isNotNull()
+	}
+
+	@Test
+	fun findAllRetroBoardsTest() {
+		val retroBoards = service.findAll()
+		Assertions.assertThat(retroBoards).isNotEmpty()
+		Assertions.assertThat(retroBoards).isNotEmpty()
+	}
+
+	@Test
+	fun cardsRetroBoardNotFoundTest() {
+		AssertionsForClassTypes.assertThatThrownBy {
+			service.findAllCardsFromRetroBoard(UUID.randomUUID())
+		}.isInstanceOf(RetroBoardNotFoundException::class.java)
+	}
+
+	@Test
+	fun findRetroBoardTest() {
+		val retroBoard = service.findById(retroBoardUUID)
+		Assertions.assertThat(retroBoard).isNotNull()
+		Assertions.assertThat(retroBoard.name).isEqualTo("Spring Boot 3.0 Meeting")
+		Assertions.assertThat(retroBoard.id).isEqualTo(retroBoardUUID)
+	}
+
+	@Test
+	fun addCardToRetroBoardTest() {
+		val card = service.addCardToRetroBoard(
+			retroBoardUUID, Card(
+				comment = "Amazing session",
+				cardType = CardType.HAPPY
+			)
+		)
+		Assertions.assertThat(card).isNotNull()
+		Assertions.assertThat(card.id).isNotNull()
+		val retroBoard = service.findById(retroBoardUUID)
+		Assertions.assertThat(retroBoard).isNotNull()
+		Assertions.assertThat(retroBoard.cards).isNotEmpty()
+	}
+
+	@Test
+	fun findAllCardsFromRetroBoardTest() {
+		val cardList = service.findAllCardsFromRetroBoard(retroBoardUUID)
+		Assertions.assertThat(cardList).isNotEmpty()
+		Assertions.assertThat((cardList as Collection<*>).size).isGreaterThan(3)
+	}
+
+	@Test
+	fun removeCardsFromRetroBoardTest() {
+		service.removeCardFromRetroBoard(retroBoardUUID, cardUUID)
+		val retroBoard = service.findById(retroBoardUUID)
+		Assertions.assertThat(retroBoard).isNotNull()
+		Assertions.assertThat(retroBoard.cards).isNotEmpty()
+		Assertions.assertThat(retroBoard.cards).hasSizeLessThan(4)
+	}
+
+	@Test
+	fun findCardByIdRetroBoardTest() {
+		val card = service.findCardByUUIDFromRetroBoard(retroBoardUUID, mehCardUUID)
+		Assertions.assertThat(card).isNotNull()
+		Assertions.assertThat(card.id).isEqualTo(mehCardUUID)
+	}
+
+	@Test
+	fun notFoundInRetroBoardTest() {
+		AssertionsForClassTypes.assertThatThrownBy {
+			service.findCardByUUIDFromRetroBoard(
+				retroBoardUUID,
+				UUID.randomUUID()
+			)
+		}.isInstanceOf(CardNotFoundException::class.java)
 	}
 
 }
